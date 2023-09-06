fd26a17450675c57d9ab2a654b36e36a42f2e022
Rishad Yasin <rishad236@gmail.com>
2023-09-06 08:39:25 -0700

    add email check to registration

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,26 @@
 const User  = require("../models/user.model")
 
 module.exports = {
-    registerUser : (req, res) => {
-        User.create(req.body)
-            .then(newUser => res.json(newUser))
-            .catch(err => res.status(400).json(err))
+    // registerUser : (req, res) => {
+    //     User.findOne({email: req.body.email})
+    //         .then(potentialUser => {
+    //             if (potentialUser) {res.status(400).json("This email already exists. Please login")}
+    //             })
+    //             User.create(req.body)
+    //                     .then(newUser => res.json(newUser))
+    //                     .catch(err => res.status(400).json(err))
+    // }
+    registerUser: async (req, res) => {
+        try{
+            const potentialUser = await User.findOne({email: req.body.email});
+            if (potentialUser) {res.status(400).json("This email already exists. Please login")}
+            else {
+                const newUser = await User.create(req.body);
+                res.json(newUser);
+            }
+        }
+        catch(err){
+            res.status(400).json(err)
+        }
     }
 }
